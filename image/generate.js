2f3e643b398294926602896f44d50a1d1eb9d307
Michael Bridgen <mikeb@squaremobius.net>
2020-04-18 17:07:16 +0100

    Make script generate a fixed point
    When called with
    
        kpt fn run .
    
    the script in the container image will receive a functionConfig with
    the configmap. If it fails to output that configmap, the file will be
    removed. So the script must include it in the output, making sure it
    doesn't mutate it unnecessarily.
    
    There's another couple of modes the script can be run in:
    
     - on its own, or piping `kpt fn source .` into it;
     - with `kpt fn run . --image -- item=value`
    
    For the first form, the script constructs a default `functionConfig`.
    For the second, it accepts the `functionConfig` it gets, but renames
    it so it'll be put in the expected file.
    
    This should mean that running `kpt fn run .` without changing anything
    will not make changes to any files.

--- image/generate.js
+++ image/generate.js
@@ -1,5 +1,11 @@
 import { core, apps } from '@jkcfg/kubernetes/api';
 import { log, read, write, stdin, stdout, Format } from '@jkcfg/std';
+import { stringify } from '@jkcfg/std';
+import { merge } from '@jkcfg/std/merge';
+
+// This script can be run without building the image:
+//
+//    kpt source . | jk --lib jkcfg/kubernetes:0.6.2 ./image/generate.js
 
 class ResourceList {
   constructor(items) {
@@ -9,16 +15,87 @@ class ResourceList {
   }
 }
 
+const app = 'helloworld';
+const functionConfigName = 'generate';
+const generateImage = 'generate';
+
 async function main() {
   const input = await read(stdin, { format: Format.YAML });
 
+  // If run using `kpt fn run . --image=generate`, this'll get a
+  // functionConfig made for it by `kpt fn`. I can use that as a
+  // bootstrapping mechanism, by including it in the resources output
+  // so it's written to the directory. The generation step can then be
+  // invoked with `kpt fn run .` the next time.
+  const defaultData = {
+    namespace: 'default',
+    image: 'helloworld',
+  };
+
+  // If run using `kpt source . | jk run ./image/generate.js` there
+  // will be no function config; this constructs a default for that
+  // situation.
+  const defaultFunctionConfig = new core.v1.ConfigMap(functionConfigName, {
+    metadata: {
+      annotations: {
+        'config.k8s.io/function': stringify({
+          container: {
+            image: generateImage,
+          },
+        }, Format.YAML),
+      },
+    },
+    data: defaultData,
+  });
+
+  const { functionConfig = defaultFunctionConfig } = input;
+  const config = Object.assign({}, defaultData, functionConfig.data || {});
+  const { namespace, image } = config;
+
+  log(config);
+
+  // unconditionally set the functionConfig name to something I like
+  const functionConfigResource = merge(functionConfig, {
+    metadata: {
+      name: functionConfigName,
+    },
+  });
+
   const items = [
-    new apps.v1.Deployment('deploy', {
+    new apps.v1.Deployment('helloworld', {
+      metadata: { namespace },
+      spec: {
+        selector: {
+          matchLabels: { app },
+        },
+        template: {
+          metadata: {
+            labels: { app },
+          },
+          spec: {
+            containers: [
+              { image }
+            ],
+          },
+        },
+      },
     }),
-    new core.v1.Service('srv', {
+
+    new core.v1.Service('helloworld', {
+      metadata: { namespace },
+      spec: {
+        selector: {
+          matchLabels: { app },
+        },
+        ports: [
+          { port: 80 },
+        ],
+      },
     }),
+
+    functionConfigResource,
   ];
-  const rl = new ResourceList([...items, ...(input) ? input.items : []]);
+  const rl = new ResourceList(items);
   log(rl);
   write(rl, stdout, { format: Format.YAML });
 }
